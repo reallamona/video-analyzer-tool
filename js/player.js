3e35d016bb79b53a94cb18e15995b74ed3795ba8
Lamona <lamona.content@gmail.com>
2026-08-01 04:40:36 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -52,22 +52,44 @@ function loadVideo(event) {
 
 
 /* =========================
-   Time Formatter
+   Time Format
 ========================= */
 
 function formatTime(seconds) {
 
-    seconds = Math.floor(seconds);
+    if (isNaN(seconds)) {
+        return "00:00:00:000";
+    }
 
-    const minutes = Math.floor(seconds / 60);
 
-    const remainingSeconds = seconds % 60;
+    const hours = Math.floor(seconds / 3600);
+
+    const minutes = Math.floor(
+        (seconds % 3600) / 60
+    );
+
+    const secs = Math.floor(
+        seconds % 60
+    );
+
+    const milliseconds = Math.floor(
+        (seconds % 1) * 1000
+    );
 
 
     return (
-        String(minutes).padStart(2, "0") +
-        ":" +
-        String(remainingSeconds).padStart(2, "0")
+
+        String(hours).padStart(2, "0")
+        + ":"
+        +
+        String(minutes).padStart(2, "0")
+        + ":"
+        +
+        String(secs).padStart(2, "0")
+        + ":"
+        +
+        String(milliseconds).padStart(3, "0")
+
     );
 
 }
