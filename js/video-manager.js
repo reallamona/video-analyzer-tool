84cc08a375f1176efd8e47352cb8ea4a68da56c6
Lamona <lamona.content@gmail.com>
2026-08-04 04:31:05 -0500

    Update video-manager.js

--- js/video-manager.js
+++ js/video-manager.js
@@ -1,1 +1,26 @@
+/* =========================
+   Video Manager
+========================= */
 
+const VideoManager = {
+
+    video: null,
+
+    youtubePlayer: null,
+
+    type: null,
+
+
+    initialize() {
+
+        this.video =
+            document.getElementById("video");
+
+
+        console.log(
+            "Video Manager Initialized"
+        );
+
+    }
+
+};
